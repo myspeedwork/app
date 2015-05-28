2d6acf83b645f02ad2b1ef9b7c09b9391cd5df71
sankar <sankar.suda@gmail.com>
2015-05-28 16:07:56 +0530

    cleanup

--- system/config/config.php
+++ system/config/config.php
@@ -1,13 +1,6 @@
 <?php
 
-$_config['use_power'] = true;
-
 $_config['app']['name'] = 'Speedwork';
-
-$_config['seo'] = [
-    'seo'    => ['enable' => true],
-    'router' => ['enable' => true],
-];
 
 $_config['public_permissions'] = [
     'cron:**',
@@ -15,7 +8,7 @@ $_config['public_permissions'] = [
 
 $_config['mail'] = [
     'enable'     => false,
-    'blacklist'  => true,
+    'blacklist'  => false,
     'from_name'  => 'Speedwork',
     'from_email' => 'no-reply@speedwork.com',
     'log'        => true,
@@ -35,19 +28,6 @@ $_config['members']['default_user_group']         = 3;
 $_config['members']['enable_online_registration'] = true;
 $_config['members']['activation']                 = false;
 
-$_config['Security']['cipherSeed'] = 'DYhG93b0qyJfIxfdfers2gufehVoUub';
-$_config['salting']                = true;
-
-//fkuserid tables
-$_config['event_tables'] = [
-    'api_users',
-    'user_login_history',
-    'user_options',
-    'whitelist_domains',
-    'user_social',
-    'user_contact_details',
-];
-
 //creating the system config
 $_config['theme_config'] = [
     'default'  => 'volt',
@@ -59,13 +39,7 @@ $_config['theme_config'] = [
     ],
 ];
 
-//$_config['offline'] = json_decode('{"is_offline":"1","key":"sinfini","theme":"","offline_msg":"This site is presently in updating stage.<br\/>We will be back soon. Sorry for inconvenience..."}', true);
-
-$_config['on_login']     = '/index.php';
-$_config['after_logout'] = '/index.php';
-
-Configure::write('Cache.disable', false);
-
-if (!defined('_IMG_URL') && defined('MURL')) {
-    define('_IMG_URL', MURL.'/public/uploads/');
+$env = Configure::read('environment');
+if ($env && $file = file_exists(__DIR__.'/config_'.$env.'.php')) {
+    require $file;
 }
