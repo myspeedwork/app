1e1bcb3e83dc436c6b2b2eadbd53fd983ee8c2f9
sankar <sankar.suda@gmail.com>
2016-01-22 17:08:06 +0530

    schema change

--- schema/users.sql
+++ schema/users.sql
@@ -28,7 +28,6 @@ CREATE TABLE IF NOT EXISTS `speed_users` (
   `password` varchar(255) NOT NULL,
   `token` varchar(150) NOT NULL,
   `first_name` varchar(100) NOT NULL,
-  `middle_name` varchar(100) NOT NULL,
   `last_name` varchar(100) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `mobile` varchar(15) NOT NULL,
